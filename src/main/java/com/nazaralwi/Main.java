21fbcd514c9e5dd4346b365859f2984c838daf24
Nazar Alwi <alwinazar75@gmail.com>
2021-06-23 12:00:31 +0700

    Remove unnecessary import

--- src/main/java/com/nazaralwi/Main.java
+++ src/main/java/com/nazaralwi/Main.java
@@ -1,7 +1,5 @@
 package com.nazaralwi;
 
-import java.util.HashMap;
-
 public class Main {
     public static void main(String[] args) {
         MyHashMap<String, String> hashMap = new MyHashMap<>();
